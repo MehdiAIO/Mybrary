0803afcfde07e66a6ef2aeb6581cea5ff6e4fa51
MehdiAIO <elmahdi.aitouzdi@gmail.com>
2024-04-13 13:34:14 +0000

    error message

--- routes/authors.js
+++ routes/authors.js
@@ -1,39 +1,40 @@
 const express = require("express");
 
 // importing author model
-const Author=require('../models/author');
+const Author = require("../models/author");
 
 // Create an instance of the express Router class.
 const router = express.Router();
 
 // All authors Endpoint
 router.get("/", (req, res) => {
-  res.render("authors/index");
+  res.render("authors/index", { errorMessage: "" });
 });
 
 // New Author Endpoint
 router.get("/new", (req, res) => {
-  res.render("authors/new",{author: new Author()}); // empty object for form
+  res.render("authors/new", { author: new Author(), errorMessage: "" }); // empty object for form
 });
 
 // Create New Author
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const author = new Author({
-    name: req.body.name
+    name: req.body.name,
   });
-
-  author.save()
-    .then((newAuthor) => {
-      res.redirect('/authors');
-    })
-    .catch((err) => {
-      res.render('authors/new', {
-        author: author,
-        message: err.message
-      });
+  try {
+    const newAuthor = await author.save();
+    res.redirect("/authors");
+  } catch (error) {
+    let errorMessage = "Error creating author";
+    if (error.code === 11000) { // Duplicate key error
+      errorMessage = "Author with this name already exists";
+    }
+    res.render("authors/new", {
+      author: author,
+      errorMessage: errorMessage,
     });
+  }
 });
 
 
-// Exporting the module so it can be used in other files
 module.exports = router;
